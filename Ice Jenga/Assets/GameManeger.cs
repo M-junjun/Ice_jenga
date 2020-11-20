19e531630df9b708713328e6caeab84ae330a7a8
shoichiro ito <x3033010@edu.gifu-u.ac.jp>
2020-11-20 17:36:05 +0900

    ジェンガ配列完了(^^♪

---
+++ Ice Jenga/Assets/GameManeger.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManeger : MonoBehaviour
+{
+    public GameObject obj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float X = 0; //ジェンガの中心x座標
+        float Y = 0; //ジェンガの中心y座標
+        float Z = 0; //ジェンガの中心z座標
+
+        for (int y = 0; y < 12; y++)
+        {
+            for (int x = -1; x < 2; x++)
+            {
+                // プレハブを元にオブジェクトを生成する
+                if (y % 2 == 0)
+                {
+                    GameObject instance =
+                    (GameObject)Instantiate(obj, new Vector3(6.0f * x, 4.0f*y, 0.0f), Quaternion.identity);
+                }
+                else
+                {
+                    GameObject instance =
+                    (GameObject)Instantiate(obj, new Vector3(0.0f, 4.0f*y, 6.0f * x), Quaternion.Euler(0f,90f,0f));
+
+                }
+
+            }
+
+        }
+        
+    }
+       
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
