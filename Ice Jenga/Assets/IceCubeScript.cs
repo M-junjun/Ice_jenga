a774e0f51a9a879460f46b00886acab346fd4cc5
Junya Mishima <jun.m.0812@gmail.com>
2020-12-04 15:26:01 +0900

    氷の解ける処理の追加

---
+++ Ice Jenga/Assets/IceCubeScript.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceCubeScript : MonoBehaviour
+{
+    private float melt = -2;
+    private const float MELTMAX = 100;
+    private Vector3 size; 
+
+    public float Melt { get => melt;}
+    public void MeltAdd(float value)
+    {
+        melt += value;
+    }
+    private System.Random random = new System.Random();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        size = new Vector3
+            (this.transform.localScale.x,
+            this.transform.localScale.y,
+            this.transform.localScale.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (melt > 0 && random.NextDouble() > 0.999) {
+            this.transform.localScale = size * (MELTMAX - melt) / MELTMAX;
+            melt += 0.002f;
+        }
+        if(melt <= 0)
+        {
+            melt += 0.001f;
+        }
+    }
+}
