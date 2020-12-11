7920372b5c0fde8e83ce904f095b98f43d51fa98
Junya Mishima <jun.m.0812@gmail.com>
2020-12-11 14:26:50 +0900

    太陽光の処理のための粒子作成

---
+++ Ice Jenga/Assets/SunScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunScript : MonoBehaviour
+{
+    public GameObject obj;
+    public Vector3 V;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float X = this.transform.position.x;
+        float Y = this.transform.position.y;
+        float Z = this.transform.position.z;
+        GameObject instance = (GameObject)Instantiate(obj, new Vector3(X, Y, Z), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += V;
+    }
+}
